2cf18906daf7ae1ea17ce7194c10b33ac9bb8a9a
rudrakumar2012 <manaw10071994@gmail.com>
2024-01-30 19:58:45 +0530

    contract added and deployed on localhost and hardhat config changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const chai = await hre.ethers.deployContract("chai");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await chai.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Contracted deployed to ${chai.target}`
   );
 }
 
@@ -23,3 +14,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// contract address: 0x5FbDB2315678afecb367f032d93F642f64180aa3
